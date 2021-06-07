6ce7208ece0c47aaa840216b56137253bfc64e7a
Ученик <8887081-allyru0@users.noreply.gitlab.com>
2021-06-07 20:55:10 +0300

    Добавлен функционал вывода достопримечательностей

---
+++ app/src/main/java/ru/mirea/tourist/Model/City.java
@@ -0,0 +1,21 @@
+package ru.mirea.tourist.Model;
+
+public class City {
+    public String lions;
+
+    public City(){
+
+    }
+
+    public City(String lions) {
+        this.lions = lions;
+    }
+
+    public String getLions() {
+        return lions;
+    }
+
+    public void setLions(String lions) {
+        this.lions = lions;
+    }
+}
--- app/src/main/java/ru/mirea/tourist/Search.java
+++ app/src/main/java/ru/mirea/tourist/Search.java
@@ -2,11 +2,30 @@ package ru.mirea.tourist;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ru.mirea.tourist.Model.City;
+import ru.mirea.tourist.Model.Users;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +42,12 @@ public class Search extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private List<String> listData;
+    private DatabaseReference mDataBase;
+    private String City_name;
+    private Spinner spinner;
 
     public Search() {
         // Required empty public constructor
@@ -60,6 +85,49 @@ public class Search extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_search2, container, false);
+
+        Button search_information = (Button) view.findViewById(R.id.search_btn);
+        spinner = view.findViewById(R.id.spinnerCity);
+
+        listView = view.findViewById(R.id.list_information);
+        listData = new ArrayList<>();
+        adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1,listData);
+        listView.setAdapter(adapter);
+
+        search_information.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                City_name = spinner.getSelectedItem().toString();
+                mDataBase = FirebaseDatabase.getInstance("https://tourist-3be36-default-rtdb.europe-west1.firebasedatabase.app/").getReference(City_name);
+                getDataFromDB();
+            }
+        });
+
+
         return view;
     }
+
+    private void getDataFromDB(){
+        ValueEventListener vListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull  DataSnapshot snapshot) {
+                if(listData.size() > 0) listData.clear();
+
+                for(DataSnapshot ds : snapshot.getChildren()){
+                    City city = ds.getValue(City.class);
+                    assert city != null;
+                    listData.add(city.lions);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull  DatabaseError error) {
+
+            }
+        };
+        mDataBase.addValueEventListener(vListener);
+    }
+
+
 }
